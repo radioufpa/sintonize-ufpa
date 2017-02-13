8be65b9b3220e8c91b271498c57138e7079b36a7
Lucas Mauess <lucasmauess@outlook.com>
2017-02-13 16:01:54 -0200

    v1

---
+++ index.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+
+	<?php 
+
+	$page_name = 'Sintonize UFPA';
+	$page_description = 'Quadro de reportagens da Rádio Web UFPA e Academia Amazônia para a Rádio e TV Cultura do Pará, sobre a produção cientifica da Universidade Federal do Pará.';
+	$page_tags = 'sintonize ufpa, sintonize radio web, sintonize academia amazonia, sintonize tv cultura, sintonize radio cultura';
+	$color = '#376b2e';
+	$image_icon = 'http://radio.ufpa.br/sintonize-ufpa/_images/icon.png';
+	$image_share = 'http://radio.ufpa.br/sintonize-ufpa/_images/share.png';
+
+	include '../_includes/head.php';
+
+	?>
+
+	<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+	<?php 
+
+	include '../_includes/header.php'; 
+
+	include '../_includes/share.php';
+	 
+	?>
+
+
+    <section class="full-screen img-1">
+
+    	<div class="full-screen-container color-4">
+
+    		<img src="_images/sintonize-ufpa-logo.svg" width="720px" alt="logo sintonize ufpa">
+
+	        <!-- <h1>Sintonize UFPA</h1> -->
+
+	        <a href="#apresentacao" id="scroll-button" class="color-1"></a>
+
+		</div>
+
+    </section>
+
+
+    <section id="apresentacao" class="full-screen img-3">
+
+        <div class="full-screen-container invert featured-text color-3">
+	   		<h2>Sintonize UFPA: a universidade perto de você</h2>
+
+	   		<p>O Sintonize UFPA é um quadro de reportagens produzidas pela Rádio Web UFPA e pelo Academia Amazônia para a Rádio e TV Cultura do Pará. O programa tem como destaque a produção cientifica da Universidade Federal do Pará.</p>
+
+			<p>Os programas são veiculados semanalmente nas emissoras Funtelpa - às quintas-feiras no intervalo do Jornal da Manhã, da Cultura FM, e às sextas-feiras durante o Jornal Cultura, na TV.</p>
+
+			<p>Aqui você confere na íntegra todos os programas.</p>
+        </div>
+
+	</section>
+
+
+    <section class="bg-gray">
+
+    	<h2 class="text-1">Programas</h2>
+
+    	<div class="card">
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('_images/gestao-publica.jpg');"></div>
+				<h3>Gestão Pública</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('_images/moradia-cidada.jpg');"></div>
+				<h3>Moradia Cidadã</h3>
+			</a>
+
+        	<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Laboratório de Engenhocas</h3>
+			</a>
+
+        	<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Ajude com Palavras</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Facom 40 Anos</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Tribologia</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>TAMBOR</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>SIDOP</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Sítio Escola Engenho do Murutucu</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Lacore</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Ocupa Belém</h3>
+			</a>
+
+    	    <a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Educação Financeira</h3>
+			</a>
+
+    		<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Enfermágicos</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Educação Carcerária</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Castanhal Esporte Clube</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Desafio Inove Mais</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Aproveitamento da Água da Chuva</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Jornada de Extensão</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Clube de Ciências de Castanhal</h3>
+			</a>			
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Exposição Beira do Rio 30 anos</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Feira do Vestibular UFPA</h3>
+			</a>
+
+			<a href="#" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Roteiro Geo-Turístico</h3>
+			</a>	
+
+			<a href="http://radio.ufpa.br/play/programas/sintonize-ufpa/2016/aulao-solidario/" class="newWindow">
+				<div style="background-image: url('');"></div>
+				<h3>Aulão Solidário</h3>
+			</a>
+
+			<a href="http://radio.ufpa.br/play/programas/sintonize-ufpa/2016/centro-de-informacao-e-memoria-da-pessoa-surda" class="newWindow">
+				<div style="background-image: url('#');"></div>
+				<h3>Centro de Informação e Memória da Pessoa Surda</h3>
+			</a>
+
+    	</div>
+
+    </section>
+
+	<?php include '../_includes/footer.php'; ?>
+		
+<!-- Scripts -->
+<script src="http://radio.ufpa.br/_scripts/functions.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/menu.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/header.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/scroll.js" type="text/javascript"></script>
+<script src="http://radio.ufpa.br/_scripts/newwindow.js" type="text/javascript"></script>
+
+</body>
+</html>
